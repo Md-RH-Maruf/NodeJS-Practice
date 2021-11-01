af5c71d41ede98b7bd4efd626ba83b2580ba6785
Md-RH-Maruf <mdrobiulhasan2620@gmail.com>
2021-11-02 01:15:47 +0600

    Token Authentication Complete in Raw NodeJs Project

--- Learn With Sumit/Raw-Node-Project/handlers/routeHandlers/userHandler.js
+++ Learn With Sumit/Raw-Node-Project/handlers/routeHandlers/userHandler.js
@@ -1,6 +1,7 @@
 
-const { hash } = require('../../helpers/utilities');
+const { hash, parseJSON } = require('../../helpers/utilities');
 const data = require('../../lib/data');
+const  tokenHandler  = require('./tokenHandler');
 const handler = {};
 
 handler.userHandler = (requestProperties,callback) => {
@@ -10,9 +11,7 @@ handler.userHandler = (requestProperties,callback) => {
     }else {
         callback(405);
     }
-    callback(200,{
-        message: 'This is a user url',
-    })
+    
 }
 
 handler._users = {};
@@ -22,7 +21,7 @@ handler._users.post = (requestProperties, callback) => {
     const lastName = typeof(requestProperties.body.lastName) === 'string' &&  requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName.trim() : false;
     const phone = typeof(requestProperties.body.phone) === 'string' &&  requestProperties.body.phone.trim().length > 0 ? requestProperties.body.phone.trim() : false;
     const password = typeof(requestProperties.body.password) === 'string' &&  requestProperties.body.password.trim().length > 0 ? requestProperties.body.password.trim() : false;
-    const tosAgreement = typeof(requestProperties.body.tosAgreement) === 'boolean' &&  requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.tosAgreement.trim() : false;
+    const tosAgreement = typeof(requestProperties.body.tosAgreement) === 'boolean' ? requestProperties.body.tosAgreement : false;
 
     if(firstName && lastName && phone && password && tosAgreement){
         data.read('users',phone, (err) => {
@@ -58,15 +57,139 @@ handler._users.post = (requestProperties, callback) => {
 };
 
 handler._users.get = (requestProperties, callback) => {
-    callback(200,{});
+    const phone = typeof(requestProperties.queryStringObject.phone) === 'string' &&  requestProperties.queryStringObject.phone.trim().length > 0 ? requestProperties.queryStringObject.phone.trim() : false;
+
+    if(phone){
+        let token = typeof(requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token: false;
+
+        tokenHandler._token.verify(token,phone, (tokenId)=>{
+            if(tokenId){
+                data.read('users',phone,(err,u) =>{
+                    const user = {...parseJSON(u)};
+                    if(!err && user){
+                        delete(user.password);
+                        callback(200,user);
+                    }else {
+                        callback(404,{
+                            error: 'Requested user was not found!',
+                        });
+                    }
+                });
+            }else{
+                callback(403,{
+                    error: 'Authentication Failure',
+                });
+            }
+        });
+        
+    }else{
+        callback(404,{
+            error: 'Requested user was not found!',
+        })
+    }
 };
 
 handler._users.put = (requestProperties, callback) => {
+    const firstName = typeof(requestProperties.body.firstName) === 'string' &&  requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName.trim() : false;
+    const lastName = typeof(requestProperties.body.lastName) === 'string' &&  requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName.trim() : false;
+    const phone = typeof(requestProperties.body.phone) === 'string' &&  requestProperties.body.phone.trim().length > 0 ? requestProperties.body.phone.trim() : false;
+    const password = typeof(requestProperties.body.password) === 'string' &&  requestProperties.body.password.trim().length > 0 ? requestProperties.body.password.trim() : false;
+    
+    if(phone){
+        if(firstName || lastName || password){
+            let token = typeof(requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token: false;
 
+        tokenHandler._token.verify(token,phone, (tokenId)=>{
+            if(tokenId){
+                data.read('users',phone,(err, uData)=>{
+                    let userData = {...parseJSON(uData)};
+                    if(!err && userData){
+                        if(firstName){
+                            userData.firstName = firstName;
+                        }
+                        if(lastName){
+                            userData.lastName = lastName;
+                        }
+                        if(password){
+                            userData.password = hash(password);
+                        }
+    
+                        data.update('users',phone,userData,(err2)=>{
+                            if(!err2){
+                                callback(200,{
+                                    message: 'User was updated usccessfully!',
+                                })
+                            }else{
+                                callback(500,{
+                                    error: 'There was a problem in the server side!'
+                                })
+                            }
+                        })
+                    }else{
+                        callback(400,{
+                            error: "You have a problem in your request!",
+                        })    
+                    }
+                });
+            }else{
+                callback(403,{
+                    error: 'Authentication Failure',
+                });
+            }
+        });
+            
+        }else{
+            callback(400,{
+                error: "You have a problem in your request!",
+            })    
+        }
+    }else{
+        callback(400,{
+            error: "Invalid phone number, Please try again!",
+        })
+    }
 };
 
 handler._users.delete = (requestProperties, callback) => {
+    const phone = typeof(requestProperties.queryStringObject.phone) === 'string' &&  requestProperties.queryStringObject.phone.trim().length > 0 ? requestProperties.queryStringObject.phone.trim() : false;
 
+    if(phone){
+        let token = typeof(requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token: false;
+
+        tokenHandler._token.verify(token,phone, (tokenId)=>{
+            if(tokenId){
+                data.read('users',phone,(err,userData) =>{
+                    if(!err && userData){
+                        data.delete('users',phone,(err2)=>{
+                            if(!err2){
+                                callback(200,{
+                                    message: "User was deleted successfully"
+                                });
+                            }else{
+                                callback(500,{
+                                    error: 'There was a server side error!'
+                                })
+                            }
+                        });
+                    }else{
+                        callback(500,{
+                            error: 'There was a server side error!'
+                        })        
+                    }
+                });
+            }else{
+                callback(403,{
+                    error: 'Authentication Failure',
+                });
+            }
+        });
+        
+    }else{
+        callback(400,{
+            error: 'There was a problem in your request!'
+        })
+    }
 };
+
 
 module.exports = handler;
--- Learn With Sumit/Raw-Node-Project/helpers/utilities.js
+++ Learn With Sumit/Raw-Node-Project/helpers/utilities.js
@@ -24,4 +24,18 @@ utilities.hash = (str) => {
     return false;
 };
 
+utilities.createRandomString = (stringLength) =>{
+    let length = stringLength;
+
+    length = stringLength> 0? stringLength: false;
+    let output = '';
+    if(length){
+        let acceptableCharacter = "abcdefghijklmnopqrstuvwxyz1234567890";
+        for(let i=1;i<=stringLength;i++){
+            output += acceptableCharacter.charAt(Math.floor(Math.random()*35));
+        }
+    }
+    return output;
+}
+
 module.exports = utilities;
